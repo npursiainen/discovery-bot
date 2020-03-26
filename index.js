e40e4e84c4327d9cabdb4613bbf9531e65d973bc
Morgenne Besenschek <morgenne_besenschek@cooperators.ca>
2020-03-26 11:33:30 -0400

    Implemented WIP ability to request and display Discovery results through user input

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 require('./config/express')(app);
 const AssistantV2 = require('ibm-watson/assistant/v2');
+const DiscoveryV1 = require('ibm-watson/discovery/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 // declare Watson Assistant service
@@ -12,6 +13,15 @@ const assistant = new AssistantV2({
     apikey: process.env.ASSISTANT_IAM_APIKEY,
   }),
   url: process.env.ASSISTANT_URL,
+});
+
+// declare Discovery service
+const discovery = new DiscoveryV1({
+  version: '2019-02-28',
+  authenticator: new IamAuthenticator({ 
+    apikey: process.env.DISCOVERY_IAM_APIKEY
+  }),
+  url: process.env.DISCOVERY_URL
 });
 
 const date = new Date();
@@ -44,12 +54,35 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/message', (req, res) => {
+  const missingVariables = [];
   // check for assistant id and handle null assistant env variable
   const assistantId = process.env.ASSISTANT_ID || '<assistant-id>';
   if (!assistantId || assistantId === '<assistant-id>') {
     return res.json({
       output: {
         text: 'The app has not been configured with a ASSISTANT_ID environment variable.',
+      },
+    });
+  }
+
+  // TODO: Consider moving this into Discovery function or turning it into a warning message?
+  const discoveryCollectionId = process.env.DISCOVERY_COLLECTION_ID || '<discovery-id>';
+  const discoveryEnvironmentId = process.env.DISCOVERY_ENVIRONMENT_ID || '<environment-id>';
+
+  if (!discoveryCollectionId || discoveryCollectionId === '<discovery-id>') {
+    missingVariables.push('The app has not been configured with a DISCOVERY_COLLECTION_ID environment variable.');
+  }
+
+  if (!discoveryEnvironmentId || discoveryEnvironmentId === '<environment-id>') {
+    missingVariables.push('The app has not been configured with a DISCOVERY_ENVIRONMENT_ID environment variable.');
+  }
+
+  if (missingVariables.length !== 0) {
+    const error = missingVariables.join(' ');
+    console.log(error);
+    return res.json({
+      output: {
+        text: error
       },
     });
   }
@@ -67,6 +100,9 @@ app.post('/api/message', (req, res) => {
     input: {
       message_type: 'text',
       text: textIn,
+      options: {
+        return_context: true
+      }
     },
   };
 
@@ -74,16 +110,80 @@ app.post('/api/message', (req, res) => {
     payload.context = req.body.context || initContext;
   }
 
-  // send payload to Conversation and return result
-  return assistant.message(payload, (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(err.code || 500).json(err);
-    }
-    console.log(JSON.stringify(data, null, 4));
-    return res.json(data);
-  });
+  if (payload.input.text.match('discovery')) {
+    console.log('calling Discovery...');
+    getDiscoveryResults(textIn, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(err.code || 500).json(err);
+      } else {
+        console.log('Success');
+        return res.json(data);
+      }
+    });
+  } else {
+    // send payload to Conversation and return result
+    return assistant.message(payload, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(err.code || 500).json(err);
+      }
+      console.log(JSON.stringify(data, null, 4));
+      return res.json(data);
+    });
+  }
+
 });
+
+function getDiscoveryResults(query, callback) {
+  const queryParams = {
+    environmentId: process.env.DISCOVERY_ENVIRONMENT_ID,
+    collectionId: process.env.DISCOVERY_COLLECTION_ID,
+    naturalLanguageQuery: query
+  };
+
+  return discovery.query(queryParams, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      // Converts Discovery API response
+      let botResponse;
+      if (data.result.results.length > 0) {
+        data.result.results.forEach((result) => {
+          // Depending on the data, a result may have a subtitle and no title, or vice versa
+          if (!result.hasOwnProperty('title')) {
+            const title = result.hasOwnProperty('subtitle') ? result.subtitle : [''];
+            Object.defineProperty(result, 'title', {
+              value: title,
+              enumerable: true
+            });
+          }
+        });
+        botResponse = 'Here is what I could find:'
+      } else {
+        botResponse = 'Sorry, I couldn\'t find anything that matched your query.'
+      }
+
+      const response = {
+        status: data.status,
+        statusText: data.statusText,
+        headers: data.headers,
+        result: {
+          output: {
+            generic: [{
+              header: botResponse,
+              response_type: 'search',
+              results: data.result.results
+            }]
+          }
+        }
+      };
+
+      callback(null, response);
+    }
+  });
+}
+
 
 app.get('/api/session', (req, res) => {
   assistant.createSession(
--- src/components/SearchResults/SearchResultsList.js
+++ src/components/SearchResults/SearchResultsList.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchResultsList = ({ res }) => (
-  <div>
-    {res.slice(0, 3).map(value => (
-      <div id="search-div" className="search-item">
-        <strong>{value.title}</strong>
-        <br />
-        {(value.body.length > 200) ? `${value.body.substring(0, 197)}...` : value.body}
-        <br />
-        <a href={`/search?id=${value.id}`} target="_blank" rel="noopener noreferrer">Full Document</a>
-        <div id={value.id} hidden>{value.body}</div>
-      </div>
-    ))}
-  </div>
-);
+const SEARCH_RESULT_LENGTH = 3;
+
+const SearchResultsList = ({ res }) => {
+  return (
+    <div>
+      {res.slice(0, SEARCH_RESULT_LENGTH).map(value => (
+        <div id="search-div" className="search-item">
+          <strong>{value.title[0] && value.title[0].length > 50 ? `${value.title[0].substring(0, 47)}...` : value.title[0]}</strong>
+          <br />
+          {(value.text.length > 200) ? `${value.text.substring(0, 197)}...` : value.text}
+          <br />
+          {<a target="_blank" rel="noopener noreferrer">Full Document</a>}
+          <div id={value.id} hidden>{value.text}</div>
+        </div>)
+      )}
+    </div>);
+};
 
 SearchResultsList.propTypes = {
-  res: PropTypes.object.isRequired,
+  res: PropTypes.array.isRequired,
 };
 
 export default SearchResultsList;
